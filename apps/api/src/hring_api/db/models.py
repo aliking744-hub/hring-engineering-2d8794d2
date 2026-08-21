654d3c9787a6da49aec24d190a5733773c93b46e
aliking744-hub <ali.king744@gmail.com>
2026-08-21 23:24:34 +0330

    feat: register headhunting ORM models

--- apps/api/src/hring_api/db/models.py
+++ apps/api/src/hring_api/db/models.py
@@ -3,6 +3,7 @@
 from hring_api.domains.access import models as access_models
 from hring_api.domains.admin import models as admin_models
 from hring_api.domains.ai import models as ai_models
+from hring_api.domains.headhunting import models as headhunting_models
 from hring_api.domains.identity import models as identity_models
 from hring_api.domains.identity import security_models as identity_security_models
 from hring_api.domains.identity import sms_models as identity_sms_models
@@ -12,6 +13,7 @@ __all__ = [
     "access_models",
     "admin_models",
     "ai_models",
+    "headhunting_models",
     "identity_models",
     "identity_security_models",
     "identity_sms_models",
